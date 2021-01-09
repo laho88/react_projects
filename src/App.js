85820f03d0dc8148ed3fd788d88a17831d22244f
Levi Aho <leviaho88@gmail.com>
2021-01-08 19:25:04 -0800

    initial and final commit for Props_react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from "react";
+import PersonCard from "./components/personCard.jsx";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div className = "family_list">
+    <PersonCard firstName = "Maggie" lastName = "Simpson" age = "1" hairColor = "Yellow"/>
+    <PersonCard firstName = "Yop" lastName = "Simpson" age = "12" hairColor = "Brown"/>
+    <PersonCard firstName = "Lisa" lastName = "Simpson" age = "8" hairColor = "Yellow"/>
+    <PersonCard firstName = "Bart" lastName = "Simpson" age = "10" hairColor = "Yellow"/>
+      </div>
+    )
+};
+export default App
---
+++ src/components/personCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function PersonCard(props) {
+            return(
+                <div>
+                    <h1>{props.firstName}, {props.lastName} </h1>
+                    <p>Age: {props.age}</p>
+                    <p>Hair Color: {props.hairColor}</p>
+                </div>               
+                );
+        };
+
+export default PersonCard;
